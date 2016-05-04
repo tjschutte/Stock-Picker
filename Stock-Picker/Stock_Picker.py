7d2c201840c013eebaf3d561b515ba58a495e581
Tom Schutte <tjschutte@wisc.edu>
2016-05-03 20:05:52 -0500

    First working version
    Made script utilize data it collects to calculate a few trends. From
    these trends the script makes a prediction about what the user should
    do.

--- Stock-Picker/Stock_Picker.py
+++ Stock-Picker/Stock_Picker.py
@@ -7,26 +7,16 @@ import matplotlib.ticker as mticker
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
-
-
-startdate = datetime.date(2006, 1, 1)
-today = enddate = datetime.date.today()
-ticker = 'SPY'
-
-
-fh = finance.fetch_historical_yahoo(ticker, startdate, enddate)
-# a numpy record array with fields: date, open, high, low, close, volume, adj_close)
-
-r = mlab.csv2rec(fh)
-fh.close()
-r.sort()
-# test commit comment
+import csv
+import time
 
 def moving_average(x, n, type='simple'):
     """
     compute an n period moving average.
 
     type is 'simple' | 'exponential'
+
+	x = prices
 
     """
     x = np.asarray(x)
@@ -85,123 +75,137 @@ def moving_average_convergence(x, nslow=26, nfast=12):
     emafast = moving_average(x, nfast, type='exponential')
     return emaslow, emafast, emafast - emaslow
 
+def evaluate(stock, numdays = 30):
+	'''
+	Evaluate a certian stock passed in as a string
+	Stock: The stock to look at.
+	numdays: the number of days to compare against 
+	'''
+	print("==========")
+	print("|| " + stock + " ||")
+	print("==========")
+	fh = finance.fetch_historical_yahoo(stock, startdate, enddate)
+	r = mlab.csv2rec(fh)
+	fh.close()
+	r.sort()
 
-plt.rc('axes', grid=True)
-plt.rc('grid', color='0.75', linestyle='-', linewidth=0.5)
+	prices = r.adj_close
+	# Over 70: More over 70's = over bought
+	# Under 30: More over 30's = over sold
+	overbought = False
+	oversold = False
+	over = 0
+	under = 0
+	rsi = relative_strength(prices)
+	# Itterate over the past numdays
+	for x in range(rsi.__len__() - numdays, rsi.__len__()):
+		# Detect positive slope crossing overbought line
+		if rsi[x] > 70 and rsi[x - 1] < 70: 
+			over+= 1
+		# detect negative slope crossing oversold line
+		elif rsi[x] < 30 and rsi[x-1] > 30:
+			under+= 1
+	if all(rsi[rsi.__len__()-3:] > 70):
+		overbought = True
+	elif all(rsi[rsi.__len__()-3:] < 30):
+		oversold = True
 
-textsize = 9
-left, width = 0.1, 0.8
-rect1 = [left, 0.7, width, 0.2]
-rect2 = [left, 0.3, width, 0.4]
-rect3 = [left, 0.1, width, 0.2]
+	if overbought or oversold:
+		print(stock + " is currently " + ("over bought" if overbought else "over sold"))
 
+	print("Positive slope crosses across 70: " + over.__str__())
+	print("Negative slope crosses across 30: " + under.__str__());
+	#print("RSI: \n" + str(rsi[rsi.__len__() - numdays:rsi.__len__()]))
 
-fig = plt.figure(facecolor='white')
-axescolor = '#f6f6f6'  # the axes background color
+	ma20 = moving_average(prices, 20, type='simple')
+	ma100 = moving_average(prices, 100, type='simple')
 
-ax1 = fig.add_axes(rect1, axisbg=axescolor)  # left, bottom, width, height
-ax2 = fig.add_axes(rect2, axisbg=axescolor, sharex=ax1)
-ax2t = ax2.twinx()
-ax3 = fig.add_axes(rect3, axisbg=axescolor, sharex=ax1)
+	volume = (r.close*r.volume)/1e6  # dollar volume in millions
+	vmax = volume.max()
 
+	nslow = 26
+	nfast = 12
+	nema = 9
+	emaslow, emafast, macd = moving_average_convergence(prices, nslow=nslow, nfast=nfast)
+	ema9 = moving_average(macd, nema, type='exponential') # creating the signal line
+	# macd should by the ema26 - ema12, we compare the sinal line to this. a positive slope cross signals a rise in price, we should
+	# buy while we can. a negative slope cross signals a price decline, we should sell while we can.
+	# Grab the last 30 days:
+	sigline = ema9[ema9.__len__() - numdays: ema9.__len__()]
+	sigline = [x+10 for x in sigline] # positive biasing all values, makes my buy or sell trick less sketchy
+	compline = macd[macd.__len__() - numdays: macd.__len__()]
+	compline = [x+10 for x in compline] # positive biasing all values, makes my buy or sell trick less sketchy
+	buyorsell = np.subtract(sigline, compline)
+	slopesign = 0
+	buy = False
+	sell = False
+	for x in range(numdays - 1, 0, -1):
+		if all(buyorsell[x:numdays -1] > 0) or all(buyorsell[x:numdays -1] < 0):
+			continue
+		else:
+			if buyorsell[x] > 0:
+				print("Signal to sell " + str(numdays - x) + " days ago")
+				sell = True
+				selllist.append(stock)
+			else:
+				print("Signal to buy " + str(numdays - x) + " days ago")
+				buy = True
+				buylist.append(stock)
+			break
+	if buy or sell:
+		print("Suggested action on: " + stock + " is to " + ("buy" if buy else ("sell" if sell  else "take no action")))
 
-# plot the relative strength indicator
-prices = r.adj_close
-rsi = relative_strength(prices)
-fillcolor = 'darkgoldenrod'
+# More or less what I am using as global variables
 
-ax1.plot(r.date, rsi, color=fillcolor)
-ax1.axhline(70, color=fillcolor)
-ax1.axhline(30, color=fillcolor)
-ax1.fill_between(r.date, rsi, 70, where=(rsi >= 70), facecolor=fillcolor, edgecolor=fillcolor)
-ax1.fill_between(r.date, rsi, 30, where=(rsi <= 30), facecolor=fillcolor, edgecolor=fillcolor)
-ax1.text(0.6, 0.9, '>70 = overbought', va='top', transform=ax1.transAxes, fontsize=textsize)
-ax1.text(0.6, 0.1, '<30 = oversold', transform=ax1.transAxes, fontsize=textsize)
-ax1.set_ylim(0, 100)
-ax1.set_yticks([30, 70])
-ax1.text(0.025, 0.95, 'RSI (14)', va='top', transform=ax1.transAxes, fontsize=textsize)
-ax1.set_title('%s daily' % ticker)
+startdate = datetime.date(2014, 1, 1)
+today = enddate = datetime.date.today()
+highprice = 100.0
+lowprice = 10.0
+# the amount of time in the past we want to really look at for buying, the rest is just
+# about getting more info on the stock.
+numdays = 30
 
-# plot the price and volume data
-dx = r.adj_close - r.close
-low = r.low + dx
-high = r.high + dx
+# Stocks I am 'currently' interested in.
+stocks = ['']
+# List of all the finance info on teh stocks
+stockinfo = ['']
+buylist = ['']
+selllist = ['']
 
-deltas = np.zeros_like(prices)
-deltas[1:] = np.diff(prices)
-up = deltas > 0
-ax2.vlines(r.date[up], low[up], high[up], color='black', label='_nolegend_')
-ax2.vlines(r.date[~up], low[~up], high[~up], color='black', label='_nolegend_')
-ma20 = moving_average(prices, 20, type='simple')
-ma200 = moving_average(prices, 200, type='simple')
-
-linema20, = ax2.plot(r.date, ma20, color='blue', lw=2, label='MA (20)')
-linema200, = ax2.plot(r.date, ma200, color='red', lw=2, label='MA (200)')
-
-
-last = r[-1]
-s = '%s O:%1.2f H:%1.2f L:%1.2f C:%1.2f, V:%1.1fM Chg:%+1.2f' % (
-    today.strftime('%d-%b-%Y'),
-    last.open, last.high,
-    last.low, last.close,
-    last.volume*1e-6,
-    last.close - last.open)
-t4 = ax2.text(0.3, 0.9, s, transform=ax2.transAxes, fontsize=textsize)
-
-props = font_manager.FontProperties(size=10)
-leg = ax2.legend(loc='center left', shadow=True, fancybox=True, prop=props)
-leg.get_frame().set_alpha(0.5)
-
-
-volume = (r.close*r.volume)/1e6  # dollar volume in millions
-vmax = volume.max()
-poly = ax2t.fill_between(r.date, volume, 0, label='Volume', facecolor=fillcolor, edgecolor=fillcolor)
-ax2t.set_ylim(0, 5*vmax)
-ax2t.set_yticks([])
-
-
-# compute the MACD indicator
-fillcolor = 'darkslategrey'
-nslow = 26
-nfast = 12
-nema = 9
-emaslow, emafast, macd = moving_average_convergence(prices, nslow=nslow, nfast=nfast)
-ema9 = moving_average(macd, nema, type='exponential')
-ax3.plot(r.date, macd, color='black', lw=2)
-ax3.plot(r.date, ema9, color='blue', lw=1)
-ax3.fill_between(r.date, macd - ema9, 0, alpha=0.5, facecolor=fillcolor, edgecolor=fillcolor)
-
-
-ax3.text(0.025, 0.95, 'MACD (%d, %d, %d)' % (nfast, nslow, nema), va='top',
-         transform=ax3.transAxes, fontsize=textsize)
-
-#ax3.set_yticks([])
-# turn off upper axis tick labels, rotate the lower ones, etc
-for ax in ax1, ax2, ax2t, ax3:
-    if ax != ax3:
-        for label in ax.get_xticklabels():
-            label.set_visible(False)
-    else:
-        for label in ax.get_xticklabels():
-            label.set_rotation(30)
-            label.set_horizontalalignment('right')
-
-    ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+''' Grabbing all of the companies from the NASDAQ stock exchange '''
+with open('companylist.csv') as csvfile:
+	freader = csv.reader(csvfile, delimiter=',')
+	for line in freader:
+		# Get rid of the header line, as well as any stocks with no prices...
+		if line[2] == "LastSale" or line[2] == "n/a":
+			continue
+		#Only getting companies we can afford.....
+		elif float(line[2]) < highprice and float(line[2]) > lowprice:
+			stocks.append(line[0])
 
 
-class MyLocator(mticker.MaxNLocator):
-    def __init__(self, *args, **kwargs):
-        mticker.MaxNLocator.__init__(self, *args, **kwargs)
+''' Plan:
+	-Go through stock by stock, Fet out each one, (check if the price is say between $10-$100 or something
+	-On stocks that fit this bill, we want to run our analysis
+	-find out if the company is on an up swing or down swing, and print to sell or buy IF
+		-in the last month the RSI shows that we should consider dealing with this stock.
 
-    def __call__(self, *args, **kwargs):
-        return mticker.MaxNLocator.__call__(self, *args, **kwargs)
+ '''
 
-# at most 5 ticks, pruning the upper and lower so they don't overlap
-# with other ticks
-#ax2.yaxis.set_major_locator(mticker.MaxNLocator(5, prune='both'))
-#ax3.yaxis.set_major_locator(mticker.MaxNLocator(5, prune='both'))
-
-ax2.yaxis.set_major_locator(MyLocator(5, prune='both'))
-ax3.yaxis.set_major_locator(MyLocator(5, prune='both'))
-
-plt.show()
+'''Where the Magic happens!'''
+start_time = time.time()
+for ticker in stocks:
+	if ticker != "":
+		#try:
+		evaluate(ticker, numdays)
+		#except IndexError:
+		#	print(ticker + " stock is to young / not enough data available, ignore suggestions about stock.")
+		#except Exception:
+		#	print("Error getting " + ticker + " data, ignore suggestions about stock")
+end_time = time.time()
+uptime = end_time - start_time
+print("Stock analysis done in: " + datetime.timedelta(seconds=int(uptime)).__str__())
+print("Buy these stocks: ")
+print(buylist)
+print("Sell these stocks: ")
+print(selllist)
